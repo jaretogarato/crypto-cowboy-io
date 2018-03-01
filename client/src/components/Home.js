2fd1c4a93b43d8d072baf6070e682ef759067287
Jared Gallardo <jaredgg@gmail.com>
2018-03-01 00:02:56 -0700

    add link icons to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import { Header, Container, Grid } from 'semantic-ui-react';
+import { Header, Container, Grid, Image } from 'semantic-ui-react';
 import TopBg from '../assets/images/hh-bg-home-02.png';
 import Cowboy from '../assets/images/cowboy-2.jpg';
+import IconClients from '../assets/images/icon-clients-ltblue.png';
+import IconContact from '../assets/images/icon-contact-ltblue.png';
+import IconRegulatory from '../assets/images/icon-regulatory-ltblue.png';
+import IconServices from '../assets/images/icon-services-ltblue.png';
 import Footer from './partials/Footer';
 import {
   HeroHeader,
@@ -27,13 +31,60 @@ class Home extends Component {
 
         <Container>
           <Header as='h1' /> {/* vertical whitespace */}
+          <Header as='h1' /> {/* vertical whitespace */}
           <Header as='h1' textAlign='center'>Introducing Crypto Cowboy</Header>
-          <Grid>
+          <Grid align='center' verticalAlign='middle' padded >
             <Grid.Row>
-              <Grid.Column width={16}>
+              <Grid.Column width={2} />
+              <Grid.Column width={12}>
                 <img src={Cowboy} />
-                <Header as='h3' centered>
+              </Grid.Column>
+              <Grid.Column width={2} />
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column width={1} />
+              <Grid.Column width={14}>
+                <Header as='h3' textAlign='center'>
                   By using our proprietary blockchain technology specifically designed for financial services, CryptoCowboy can take the world of contracts and certificates and turn them into an efficient, fast, and secure way of managing ownership of your assets.
+                </Header>
+              </Grid.Column>
+              <Grid.Column width={1} />
+            </Grid.Row>
+            <Header as='h1' /> {/* vertical whitespace */}
+            <Header as='h1' /> {/* vertical whitespace */}
+            <Grid.Row>
+              <Grid.Column width={4} textAlign='center'>
+                <a href='/clients'><Image src={IconClients} size='small' align='center' /></a>
+              </Grid.Column>
+              <Grid.Column width={4}>
+                <a href='/services'><Image src={IconServices} size='small' /></a>
+              </Grid.Column>
+              <Grid.Column width={4}>
+                <a href='/regulatory'><Image src={IconRegulatory} size='small' /></a>
+              </Grid.Column>
+              <Grid.Column width={4}>
+                <a href='/contact'><Image src={IconContact} size='small' /></a>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column width={4}>
+                <Header as='h2' textAlign='center'>
+                  <a href='/clients'>Clients</a>
+                </Header>
+              </Grid.Column>
+              <Grid.Column width={4}>
+                <Header as='h2' textAlign='center'>
+                  <a href='/services'>Services</a>
+                </Header>
+              </Grid.Column>
+              <Grid.Column width={4}>
+                <Header as='h2' textAlign='center'>
+                  <a href='/regulatory'>Regulatory</a>
+                </Header>
+              </Grid.Column>
+              <Grid.Column width={4}>
+                <Header as='h2' textAlign='center'>
+                  <a href='/contact'>Contact</a>
                 </Header>
               </Grid.Column>
             </Grid.Row>
